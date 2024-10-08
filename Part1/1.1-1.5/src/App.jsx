a4b463337fee1bf50fd99143f6daf99f2c28ec8f
B00608052 <47907490+B00608052@users.noreply.github.com>
2024-10-08 12:58:34 +0300

    Add files via upload
    1.1-1.5 whole steps completed

---
+++ Part1/1.1-1.5/src/App.jsx
@@ -0,0 +1,44 @@
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      { name: 'Fundamentals of React', exercises: 10 },
+      { name: 'Using props to pass data', exercises: 7 },
+      { name: 'State of a component', exercises: 14 }
+    ]
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <hr/>
+      <Total parts={course.parts} />
+    </div>
+  );
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part, index) => (
+        <Part key={index} part={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+}
+
+const Total = ({ parts }) => {
+  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <p>Number of exercises {totalExercises}</p>;
+}
+
+const Part = ({ part, exercises }) => {
+  return <p>{part} {exercises}</p>;
+}
+
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+}
+
+export default App;
