43919bf465bbf6f048965e7f72db1f17dd099ebf
B00608052 <47907490+B00608052@users.noreply.github.com>
2024-11-26 12:29:21 +0200

    3.15-3.18

---
+++ Part3_v2/3.15-3.18/models/person.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  number: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+});
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model('Person', personSchema);
